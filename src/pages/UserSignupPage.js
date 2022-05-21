383f294305f64a050428b7c3f2e60cea7b02054e
tonaak <toannguyenitit@gmail.com>
2022-05-21 23:49:20 +0700

    Add User signup page

---
+++ src/pages/UserSignupPage.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+
+export class UserSignupPage extends Component {
+    state = {
+        displayName: '',
+        username: '',
+        password: '',
+        passwordRepeat: ''
+    }
+
+    onChangeDisplayName = (event) => {
+        const value = event.target.value;
+        this.setState({ displayName: value });
+    };
+
+    onChangeUsername = (event) => {
+        const value = event.target.value;
+        this.setState({ username: value });
+    };
+
+    onChangePassword = (event) => {
+        const value = event.target.value;
+        this.setState({ password: value });
+    };
+
+    onChangePasswordRepeat = (event) => {
+        const value = event.target.value;
+        this.setState({ passwordRepeat: value });
+    };
+
+    onClickSignup = () => {
+        const user = {
+            displayName: this.state.displayName,
+            username: this.state.username,
+            password: this.state.password
+        };
+        this.props.actions.postSignup(user);
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>Sign Up</h1>
+                <div>
+                    <input placeholder='Your display name'
+                        value={this.state.displayName}
+                        onChange={this.onChangeDisplayName} />
+                </div>
+                <div>
+                    <input placeholder='Your username'
+                        value={this.state.username}
+                        onChange={this.onChangeUsername} />
+                </div>
+                <div>
+                    <input placeholder='Your password' type='password'
+                        value={this.state.password}
+                        onChange={this.onChangePassword} />
+                </div>
+                <div>
+                    <input placeholder='Repeat your password' type='password'
+                        value={this.state.passwordRepeat}
+                        onChange={this.onChangePasswordRepeat} />
+                </div>
+                <div>
+                    <button onClick={this.onClickSignup}>Sign Up</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+UserSignupPage.defaultProps = {
+    actions: {
+        postSignup: () =>
+            new Promise((resolve, reject) => {
+                resolve({});
+            })
+    }
+}
+
+export default UserSignupPage;
